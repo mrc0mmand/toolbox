8668ead701d1e0d0765e031478656c357d54476f
mrc0mmand <frantisek@sumsal.cz>
2016-07-02 22:22:46 +0200

    Get more detailed data from fetched scrobbles

--- last.fm/backup/lastfm_stats.sh
+++ last.fm/backup/lastfm_stats.sh
@@ -9,12 +9,20 @@ DATA_DIR="$1"
 FORMAT="[%s]
 \tscrobbles: %d
 \tloved: %d
-\tscrobbles:
-\t\tfirst scrobble: %s
-\t\tlast scrobble: %s
+\tscrobble info:
+\t\tfirst scrobble:
+\t\t\tdate: %s
+\t\t\tartist: %s
+\t\t\ttrack: %s
+\t\t\talbum: %s
+\t\tlast scrobble:
+\t\t\tdate: %s
+\t\t\tartist: %s
+\t\t\ttrack: %s
+\t\t\talbum: %s
 \tlast backups:
-\t\tS: %s
-\t\tL: %s\n"
+\t\tscrobbles: %s
+\t\tloved: %s\n"
 
 while read -r dir
 do
@@ -23,12 +31,19 @@ do
                    sort -r | head -n1)"
     f_loved="$(find "$dir" -type f -name "*_loved_*" |
                sort -r | head -n1)"
-    d_first="$(date --date="@$(find "$dir" -type f -name "*_scrobbles_*" |
-               sort -r | head -n1 | xargs tail -n 1 |
-               awk '{print $1; }')")"
-    d_last="$(date --date="@$(find "$dir" -type f -name "*_scrobbles_*" |
-               sort -r | head -n1 | xargs head -n 1 |
-               awk '{print $1; }')")"
+    if [[ ! -f $f_scrobbles || ! -f $f_loved ]]; then
+        echo "Incomplete data for user $user"
+        continue
+    fi
+    s_first="$(find "$dir" -type f -name "*_scrobbles_*" |
+               sort -r | head -n1 | xargs grep -v "^0.*" | tail -n 1 |
+               sed 's/\t\t/\t \t/g')"
+    s_last="$(find "$dir" -type f -name "*_scrobbles_*" |
+               sort -r | head -n1 | xargs head -n 1 | sed 's/\t\t/\t \t/g')"
+    IFS=$'\t' read -r -a a_first <<< "$s_first"
+    IFS=$'\t' read -r -a a_last <<< "$s_last"
+    d_first="$(date --date="@${a_first[0]}")"
+    d_last="$(date --date="@${a_last[0]}")"
     d_scrobbles="$(echo "$f_scrobbles" | rev | cut -d'_' -f1 | 
                    rev | xargs date --date)"
     d_loved="$(echo "$f_loved" | rev | cut -d'_' -f1 | 
@@ -36,6 +51,9 @@ do
     scrobbles="$(wc -l "$f_scrobbles" | awk '{print $1;}')"
     loved="$(wc -l "$f_loved" | awk '{print $1;}')"
 
-    printf "$FORMAT" "$user" $scrobbles $loved "$d_first" "$d_last" "$d_scrobbles" "$d_loved"
+    printf "$FORMAT" "$user" $scrobbles $loved \
+           "$d_first" "${a_first[2]}" "${a_first[1]}" "${a_first[3]}" \
+           "$d_last" "${a_last[2]}" "${a_last[1]}" "${a_last[3]}" \
+           "$d_scrobbles" "$d_loved"
 
 done < <(find "$DATA_DIR" -mindepth 1 -maxdepth 1 -type d ! -name "_oneshot")
