65fea4b822781bfc3ec54251c268d544f907723b
mrc0mmand <frantisek@sumsal.cz>
2016-07-02 20:08:24 +0200

    Simple lastexport.py wrapper

---
+++ last.fm/backup/lastfm_backup.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/bash
+
+USERS="mrc0mmand hot_spot bizon_cz chinese_soup zdena"
+TYPES="scrobbles loved"
+RC=0
+ROOTDIR="$(dirname `readlink -f $0`)"
+DATADIR="$ROOTDIR/data"
+MAXRUN="2h"
+MAXDIFF=50
+
+trap "exit 1" SIGINT SIGTERM
+
+for u in $USERS; do
+    if [[ ! -a "$DATADIR/$u" ]]; then
+        mkdir -p "$DATADIR/$u"
+    fi
+
+    TYPERC=0
+    for t in $TYPES; do
+        FILENAME="$DATADIR/$u/${u}_${t}_$(date -Iminutes)"
+        echo "Processing user $u (type: $t, destination: $FILENAME)"
+        SECONDS=0
+        timeout --foreground $MAXRUN python2.7 \
+                "$ROOTDIR/lastexport.py" -u "$u" -o "$FILENAME" -t $t
+        echo "Download finished in $SECONDS seconds"
+        if [[ $? -ne 0 || ! -s $FILENAME ]]; then
+            TYPERC=1
+        else
+            NEWCOUNT="$(wc -l "$FILENAME" | awk '{ print $1; }')"
+            OLDCOUNT="$(find "$DATADIR/$u" -name "*_${t}_*" | sort -r | head -n 1 |
+                        xargs wc -l | awk '{ print $1; }')"
+            if (( $NEWCOUNT - $OLDCOUNT < $MAXDIFF )); then
+                find "$DATADIR/$u/" -name "*_${t}_*" -mtime +14 -exec rm -f {} \;
+            else
+                echo "Scrobble difference between last two backups is too big"
+                echo -e "Old: $OLDCOUNT\nNew: $NEWCOUNT"
+                echo "Skipping old backups cleanup..."
+            fi
+        fi
+    done
+
+    if [[ $TYPERC -ne 0 && $RC -ne 0 ]]; then
+        RC=1
+    fi
+done
+
+exit $RC
