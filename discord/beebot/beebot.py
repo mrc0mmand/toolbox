c8516763feefa4d25e0217b37321a0c858b02439
Frantisek Sumsal <frantisek@sumsal.cz>
2018-01-29 18:32:08 +0100

    Reduce the autism a little bit

--- discord/beebot/beebot.py
+++ discord/beebot/beebot.py
@@ -42,11 +42,8 @@ async def on_message(message):
 
     if message.content.lower() == "ronnie":
         await client.send_message(message.channel, "Ronnie je najlepší!")
-        
-    if message.author.name == "mrc0mmand":
-        await client.add_reaction(message, poop_emoji)
-        
+
     if ("hrame" in message.content.lower()) and (message.author.name == "laurittaprobi"):
         await client.send_message(message.channel, "If only beehives were as big as Lauritta's breasts... *sigh*")
-        
+
 client.run(token)
