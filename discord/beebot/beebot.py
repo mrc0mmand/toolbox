aa3a8a95c7b7741613c538345b7b3f0c9a20e161
Frantisek Sumsal <frantisek@sumsal.cz>
2018-02-20 16:16:16 +0100

    Respond with correctly sized 'j' #2

--- discord/beebot/beebot.py
+++ discord/beebot/beebot.py
@@ -69,7 +69,7 @@ async def on_message(message):
         author, quote = random.choice(list(quotes.items()))
         await bot.send_message(message.channel, "*\"{}\"* - {}".format(quote, author))
 
-    if message.content.lower() == "j":
+    if message.content in "Jj":
         await bot.send_message(message.channel, message.content)
 
 bot.run(token)
